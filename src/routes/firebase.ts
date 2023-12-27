1d3a13f400d7eea6b1d8369c5dafc61ec5f5c9dd
sigorhub <sigor2022@naver.com>
2023-12-27 11:00:12 +0900

    firebase SDK Add

---
+++ src/routes/firebase.ts
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBFYClslT_9-N2nKQQwJyC-FvD6HSDMrHw",
+  authDomain: "ywitter-reloaded-70965.firebaseapp.com",
+  projectId: "ywitter-reloaded-70965",
+  storageBucket: "ywitter-reloaded-70965.appspot.com",
+  messagingSenderId: "1024337563360",
+  appId: "1:1024337563360:web:737e4eb507da894d8f4a85"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
